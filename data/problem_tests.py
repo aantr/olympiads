0017f9baba1e1e24044a17a13e95449a65878d19
aantr <xgamestudio8@gmail.com>
2021-05-05 19:16:22 +0500

    #

--- data/problem_tests.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy
-from sqlalchemy import orm
-from .db_session import SqlAlchemyBase
-
-
-class ProblemTests(SqlAlchemyBase):
-    __tablename__ = 'problem_tests'
-
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
--- program_testing/test_for_a+b/test.py
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-print()
-=======
-a, b = map(int, input().split())
-print(a + b)
->>>>>>> 8026d8028f59580be6bf1596844ab1c9f053b992
